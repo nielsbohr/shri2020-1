84bc8fb479ae1b573b64a4b2e085af77841c2d0a
artemkopylov04 <artemkopylov04@gmail.com>
2019-12-10 18:03:16 +0300

    pre final

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,47 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = env => {
-
-  return {
-    mode: env.production ? 'production' : 'development',
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'build/'),
-        filename: 'script.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            MiniCssExtractPlugin.loader,
-            'css-loader',
-            'postcss-loader',
-            {
-              loader: 'sass-loader',
-              options: {
-                  implementation: require('sass'),
-              }
-            }
-          ],
+module.exports = (env) => ({
+  mode: env.production ? 'production' : 'development',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'build/'),
+    filename: 'script.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg/,
+        use: {
+          loader: 'svg-url-loader',
         },
-        {
-          test: /\.svg/,
-          use: {
-              loader: 'svg-url-loader'
-          }
-        },
-      ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-          filename: 'style.css',
-          ignoreOrder: false,
-        }),
+      },
     ],
-    watch: !env.production,
-    devServer: {
-        contentBase: path.join(__dirname, 'build/'),
-        compress: true,
-        port: 9000
-    }
-  }
-};
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+      ignoreOrder: false,
+    }),
+  ],
+  watch: !env.production,
+  devServer: {
+    contentBase: path.join(__dirname, 'build/'),
+    compress: true,
+    port: 9000,
+  },
+});
