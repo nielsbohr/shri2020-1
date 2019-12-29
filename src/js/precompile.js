8d7af015119522202667a8b95cff3561a4fe1769
nielsbohr <artemkopylov04@gmail.com>
2019-12-30 00:55:26 +0300

    dev conf

--- src/js/precompile.js
+++ src/js/precompile.js
@@ -2,8 +2,11 @@ import templater from './templater';
 import index from '../pages/index.json';
 import product from '../pages/product.json';
 
+const root = document.createElement('div');
+document.body.appendChild(root);
+
 if (window.location.pathname === '/') {
-  document.getElementById('root').innerHTML = templater(index);
+  root.innerHTML = templater(index);
 } else if (window.location.pathname === '/product') {
-  document.getElementById('root').innerHTML = templater(product);
+  root.innerHTML = templater(product);
 }
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const sass = require('sass');
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './dev.js',
   output: {
     path: path.resolve(__dirname, 'build/'),
@@ -32,19 +34,19 @@ module.exports = {
       },
     ],
   },
+  watch: true,
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'style.css',
       ignoreOrder: false,
     }),
+    new HtmlWebpackPlugin()
   ],
-  watch: true,
   devServer: {
     contentBase: path.join(__dirname, 'build/'),
     compress: true,
     port: 9000,
     historyApiFallback: {
-      index: 'index.html',
       product: 'index.html',
     },
   },
